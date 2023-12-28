9a7d8ee0dd17aacc091925e62b6cb463f45ccfbe
galaxlabs <galaxylab2020@gmail.com>
2023-12-28 23:23:09 +0500

    created some more Docs

---
+++ esanha/evaleuation_app/doctype/query_types/test_query_types.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Sanha Halal and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQueryTypes(FrappeTestCase):
+	pass
