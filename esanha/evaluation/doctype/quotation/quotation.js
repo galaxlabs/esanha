7ada7be3c2699481f0f6a94f23e185780be63df9
galaxlabs <galaxylab2020@gmail.com>
2024-01-07 22:58:29 +0500

    add new f

---
+++ esanha/evaluation/doctype/quotation/quotation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Sanha Halal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Quotation", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ esanha/evaluation/doctype/quotation/test_quotation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Sanha Halal and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQuotation(FrappeTestCase):
+	pass
